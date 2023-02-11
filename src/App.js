c4170c7a05d32ef8924550a30db5278ecaddbd38
Христюк Игорь Анатольевич <me@jk-dev.ru>
2023-02-11 23:19:39 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import CostItem from "./components/CostItem";
+
+function App() {
+
+    const costs = [
+        {
+            date: new Date(2021, 2, 12),
+            description: 'Холодильник',
+            amount: 999.99,
+        },
+        {
+            date: new Date(2021, 11, 22),
+            description: 'Honor Magic Book',
+            amount: 1299.99,
+        },
+        {
+            date: new Date(2021, 6, 12),
+            description: 'Honor Magic Watch',
+            amount: 99.50,
+        }
+    ]
+
+    return (
+        <div className="App">
+            <h1>Hello React!</h1>
+            <CostItem date={costs[0].date}
+                      description={costs[0].description}
+                      amount={costs[0].amount}
+            ></CostItem>
+            <CostItem date={costs[1].date}
+                      description={costs[1].description}
+                      amount={costs[1].amount}
+            ></CostItem>
+            <CostItem date={costs[2].date}
+                      description={costs[2].description}
+                      amount={costs[2].amount}
+            ></CostItem>
+        </div>
+    );
+}
+
+export default App;
