9bbc470a18c6d3a592e4685ebcab5b3a04e66cc5
Христюк Игорь Анатольевич <me@jk-dev.ru>
2023-02-12 20:57:19 +0300

    use state base

--- src/components/costs/CostItem.js
+++ src/components/costs/CostItem.js
@@ -1,15 +1,28 @@
 import './CostItem.css';
 import CostDate from "./CostDate";
 import Card from "../common/Card";
+// import React, { useState } from "react";
 
-const CostItem = props => (
-    <Card className='cost-item'>
-        <CostDate date={props.date}/>
-        <div className='cost-item__description'>
-            <h2>{props.description}</h2>
-            <div className='cost-item__price'>${props.amount}</div>
-        </div>
-    </Card>
-);
+const CostItem = props => {
+
+    // const [description, setDescription] = useState(props.description);
+    const description = props.description;
+
+    const changeHandler = () => {
+        // setDescription("New value")
+    }
+
+    return (
+        <Card className='cost-item'>
+            <CostDate date={props.date}/>
+            <div className='cost-item__description'>
+                <h2>{description}</h2>
+                <div className='cost-item__price'>${props.amount}</div>
+            </div>
+            <button onClick={changeHandler}>Change</button>
+        </Card>
+    );
+}
+
 
 export default CostItem;
